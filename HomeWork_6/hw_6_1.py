7a6977136980936ae7add0962c5e9f00698d9a39
Pavlo Kostenko <kadr19333@gmail.com>
2018-11-13 12:06:41 +0200

    Home work 6_1

---
+++ HomeWork_6/hw_6_1.py
@@ -0,0 +1,22 @@
+""" Задание №1.
+функция что-то делает со строкой и для непечатных символов (string.whitespace)
+вызывает исключение ValueError
+"""
+from string import whitespace
+
+
+def string_processing(text, *args, **kwargs):
+    try:
+        result = text.upper()
+        for space in text:
+            if space in whitespace:
+                raise ValueError()
+
+    except Exception as error:
+        result = 'Ошибка.Incorrect value'
+    return result
+
+
+if __name__ == "__main__":
+    text = input('Input your text: ')
+    print(string_processing(text))
